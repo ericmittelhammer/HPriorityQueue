6bcf80e925c24e5df52a6db7077a4a33e9079870
Eric Mittelhammer <eric@gawker.com>
2015-05-04 15:34:34 -0400

    first unit test passing

--- src/PriorityQueue/Heap/BinomialHeap.hs
+++ src/PriorityQueue/Heap/BinomialHeap.hs
@@ -1,15 +1,18 @@
-module PriorityQueue.Heap.BinomialHeap (BinomialHeap, Tree, mergeTree) where
+module PriorityQueue.Heap.BinomialHeap (
+    BinomialHeap,
+    Tree(EmptyTree, Singleton, Node),
+    mergeTree) where
 
 import PriorityQueue.PQ
 
 type BinomialHeap a = [Tree a]
 
-data Ord a => Tree a = EmptyTree | Singleton a | Node a (BinomialHeap a) deriving (Show)
+data Ord a => Tree a = EmptyTree | Singleton a | Node a (BinomialHeap a) deriving (Show, Eq)
 
 mergeTree :: Ord a => Tree a -> Tree a -> Tree a
 mergeTree EmptyTree EmptyTree = EmptyTree
 mergeTree EmptyTree h = h
 mergeTree h EmptyTree = h
 mergeTree (Singleton v1) (Singleton v2)
-  | v1 >= v2    = Node v1 [Singleton v2]  -- eventually change this to handle min and max heaps
+  | v1 > v2    = Node v1 [Singleton v2]  -- eventually change this to handle min and max heaps
   | otherwise   = Node v2 [Singleton v1]
--- test/TestTree.hs
+++ test/TestTree.hs
@@ -1,10 +1,11 @@
 module Main(main) where
 
 import PriorityQueue.Heap.BinomialHeap
+import PriorityQueue.Heap.BinomialHeap( Tree(EmptyTree, Singleton, Node) )
 import Test.HUnit
 
 testEmpty = TestCase $ assertEqual
-  "Should get Nothing from an empty string" 1 (1)
+  "Larger node floats to the top of the tree" (mergeTree (Singleton 5) (Singleton 4)) (Node 5 [(Singleton 4)])
 -- testNegCursor = TestCase $ assertEqual
 --   "Should get Nothing when cursor is negative" Nothing ( findIdentifier "a" (-1, -1) )
 -- testComment = TestCase $ assertEqual
