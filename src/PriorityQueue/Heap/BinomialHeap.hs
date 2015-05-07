b9a603997f7e8863d2c489b60a49bd6aaf2c1770
Eric Mittelhammer <eric@gawker.com>
2015-05-07 14:02:40 -0400

    got rid of singleton.  it was redundant

--- src/PriorityQueue/Heap/BinomialHeap.hs
+++ src/PriorityQueue/Heap/BinomialHeap.hs
@@ -1,6 +1,6 @@
 module PriorityQueue.Heap.BinomialHeap (
     BinomialHeap,
-    Tree(EmptyTree, Singleton, Node),
+    Tree(EmptyTree, Node),
     mergeTree) where
 
 import PriorityQueue.PQ
@@ -9,7 +9,7 @@ import Data.Monoid
 
 type BinomialHeap a = [Tree a]
 
-data Tree a = EmptyTree | Singleton a | Node a (BinomialHeap a) deriving (Show, Eq)
+data Tree a = EmptyTree | Node a (BinomialHeap a) deriving (Show, Eq)
 
 -- instance Ord a => Ord (Tree a) where
 
@@ -19,9 +19,9 @@ instance Ord a => Monoid (Tree a) where
 
 mergeTree :: Ord a => Tree a -> Tree a -> Tree a
 
-mergeTree (Singleton v1) (Singleton v2)
-  | v1 < v2    = Node v1 [Singleton v2]  -- eventually change this to handle min and max heaps
-  | otherwise   = Node v2 [Singleton v1]
+mergeTree this@(Node thisValue thisHeap) that@(Node thatValue thatHeap) -- merge this into that
+  | thisValue < thatValue   = Node thisValue (thisHeap ++ [that])  -- eventually change this to handle min and max heaps
+  | otherwise               = Node thatValue (thatHeap ++ [this])
 
 {- mergeTree this@(Node v1 h1) that@(Node v2 h2)
   | v1 < v2 = mergeTree n2 n1
--- test/TestTree.hs
+++ test/TestTree.hs
@@ -1,13 +1,13 @@
 module Main(main) where
 
 import PriorityQueue.Heap.BinomialHeap
-import PriorityQueue.Heap.BinomialHeap( Tree(EmptyTree, Singleton, Node) )
+import PriorityQueue.Heap.BinomialHeap( Tree(EmptyTree, Node) )
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
 
 testSingleton = TestCase $ assertEqual
-  "Smaller node floats to the top of the tree" (mergeTree (Singleton 5) (Singleton 4)) (Node 4 [(Singleton 5)])
+  "Smaller node floats to the top of the tree" (mergeTree (Node 5 []) (Node 4 [])) (Node 4 [(Node 5 [])])
 -- testNegCursor = TestCase $ assertEqual
 --   "Should get Nothing when cursor is negative" Nothing ( findIdentifier "a" (-1, -1) )
 -- testComment = TestCase $ assertEqual
