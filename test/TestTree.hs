d67839c53e5b5e92ad9a3d73776a294cf21d0c76
Eric Mittelhammer <eric@gawker.com>
2015-05-07 23:01:26 -0400

    much simpler this way

--- test/TestTree.hs
+++ test/TestTree.hs
@@ -1,13 +1,13 @@
 module Main(main) where
 
 import PriorityQueue.Heap.BinomialHeap
-import PriorityQueue.Heap.BinomialHeap( Tree(EmptyTree, Node) )
+--import PriorityQueue.Heap.BinomialHeap( Tree )
 import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
 
 testSingleton = TestCase $ assertEqual
-  "Smaller node floats to the top of the tree" (mergeTree (Node 5 []) (Node 4 [])) (Node 4 [(Node 5 [])])
+  "Smaller node floats to the top of the tree" (mergeTrees (Tree 0 5 []) (Tree 0 4 [])) (Tree 1 4 [(Tree 0 5 [])])
 -- testNegCursor = TestCase $ assertEqual
 --   "Should get Nothing when cursor is negative" Nothing ( findIdentifier "a" (-1, -1) )
 -- testComment = TestCase $ assertEqual
