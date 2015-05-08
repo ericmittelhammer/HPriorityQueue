291933964d3b4cebee63516b8f01f795eb52dbd1
Eric Mittelhammer <eric@gawker.com>
2015-05-08 11:40:20 -0400

    mergeHeaps working by adding another layer of recursion cleaned up mergeTrees by renaming specifically to priority

--- src/PriorityQueue/Heap/BinomialHeap.hs
+++ src/PriorityQueue/Heap/BinomialHeap.hs
@@ -5,35 +5,20 @@ module PriorityQueue.Heap.BinomialHeap (
 
 import PriorityQueue.PQ
 
-import Data.Monoid
-
 type BinomialHeap r v = [Tree r v]
 
 data Tree r v = Tree { rank :: r, value :: v, heap :: (BinomialHeap r v) } deriving (Show, Eq)
-
-{-
-instance Ord a => Monoid (Tree a) where
-  mempty = EmptyTree
-  mappend = mergeTrees
--}
-
-{-
-instance Ord a => Monoid ([Tree a]) where
-  mempty = []
-  mappend = mergeHeaps
--}
-
 
 mergeTrees :: (Num r, Eq r, Ord v) => Tree r v -> Tree r v -> Tree r v
 
 -- merge this into that
 mergeTrees this that
     | rank this == rank that =
-        Tree (rank smaller + 1) (value smaller) (heap smaller ++ [bigger])
+        Tree (rank hp + 1) (value hp) (heap hp ++ [lp])
     | otherwise = error "Cannot merge trees of different ranks" -- (probably.  might be able to do it recursively w/ mergeHeaps...)
     where
-        (bigger, smaller) =
-            if value this > value that
+        (hp, lp) = -- hp = "higher priority", lp = "lower priority"
+            if value this < value that  -- TODO: support both min and max heaps
                 then (this, that)
                 else (that, this)
 
@@ -43,17 +28,4 @@ mergeHeaps [] y = y
 mergeHeaps (x:xs) (y:ys)
     | rank x < rank y = x : (mergeHeaps xs (y:ys))
     | rank y < rank x = y : (mergeHeaps (x:xs) ys)
-    | otherwise = mergeHeaps (mergeTrees x y : xs) ys
-
-
-{- merge (x:[]) (y:[]) = [x <> y]
-merge (x:[]) (y:ys) = x <> y : ys
-merge (x:xs) (y:[]) = x <> y : xs -}
-
-{- insert el (EmptyTree:xs) = (Node el []):xs
-insert el (x:xs) = (mergeTree (Node))
--}
-{- mergeTree this@(Node v1 h1) that@(Node v2 h2)
-  | v1 < v2 = mergeTree n2 n1
-  | otherwise =
--}
+    | otherwise = mergeHeaps [mergeTrees x y] (mergeHeaps xs ys)
