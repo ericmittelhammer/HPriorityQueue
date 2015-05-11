10243c156a3ef526f69b46006f9a4e123928255f
Eric Mittelhammer <eric@gawker.com>
2015-05-11 13:09:37 -0400

    formatting and linting and such

--- src/PriorityQueue/Heap/BinomialHeap.hs
+++ src/PriorityQueue/Heap/BinomialHeap.hs
@@ -8,41 +8,40 @@ import PriorityQueue.PQ
 
 type BinomialHeap a = [Tree a]
 
-data Tree a = Tree { rank :: Int, value :: a, heap :: (BinomialHeap a) } deriving (Show, Eq)
+data Tree a = Tree {
+    rank :: Int,
+    value :: a,
+    heap :: BinomialHeap a
+} deriving (Show, Eq)
 
 mergeTrees :: (Ord a) => Tree a -> Tree a -> Tree a
 
--- merge this into that
-mergeTrees this that
-    | rank this == rank that =
-        Tree (rank hp + 1) (value hp) (heap hp ++ [lp])
-    | otherwise = error "Cannot merge trees of different ranks" -- (probably.  might be able to do it recursively w/ mergeHeaps...)
-    where
-        (hp, lp) = -- hp = "higher priority", lp = "lower priority"
-            if value this < value that  -- TODO: support both min and max heaps
-                then (this, that)
-                else (that, this)
+mergeTrees t1 t2
+    | rank t1 == rank t2    = Tree (rank hp + 1) (value hp) (heap hp ++ [lp])
+    | otherwise                 = error "Cannot merge trees of different ranks" -- (probably.  might be able to do it recursively w/ mergeHeaps...)
+    where   -- hp = "higher priority", lp = "lower priority"
+        (hp, lp) = if value t1 < value t2  -- TODO: support both min and max heaps
+                    then (t1, t2)
+                    else (t2, t1)
 
 mergeHeaps :: (Ord a) => BinomialHeap a -> BinomialHeap a -> BinomialHeap a
 mergeHeaps x [] = x
 mergeHeaps [] y = y
 mergeHeaps (x:xs) (y:ys)
-    | rank x < rank y = x : (mergeHeaps xs (y:ys))
-    | rank y < rank x = y : (mergeHeaps (x:xs) ys)
-    | otherwise = mergeHeaps [mergeTrees x y] (mergeHeaps xs ys)
+    | rank x < rank y   = x : mergeHeaps xs (y:ys)
+    | rank y < rank x   = y : mergeHeaps (x:xs) ys
+    | otherwise         = mergeHeaps [mergeTrees x y] (mergeHeaps xs ys)
 
 minVal :: (Ord a) => BinomialHeap a -> a
-minVal h = minimum $ map (\ x -> value x) h
+minVal h = minimum $ map value h
 
 popMin :: (Ord a) => BinomialHeap a -> (a, BinomialHeap a)
 popMin (x:xs) = bubble x xs
     where
-        -- bubble :: Tree a -> BinomialHeap a -> (a, BinomialHeap a)
-        bubble m [] = (value m, heap m)
+        bubble m []     = (value m, heap m)
         bubble m (x:xs) =
-            let next = bubble newMin xs
-                (newMin, skippedTree) =
-                    if value x < value m
-                    then    (x, m)
-                    else    (m, x)
+            let next                    = bubble newMin xs
+                (newMin, skippedTree)   = if value x < value m
+                                            then    (x, m)
+                                            else    (m, x)
             in (fst next, mergeHeaps [skippedTree] (snd next))
